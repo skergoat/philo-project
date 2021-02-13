359398c8a7cd0aca93ac74ffd31878cc41c48745
Stephane <apsarawebdev@protonmail.com>
2021-02-13 16:56:09 +0700

    interruption

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -8,4 +8,17 @@ $(function() {
     }
 });
 
-    
+// order 
+$(function() {
+
+    $( ".js-like-article" ).on( "click", function(e) {
+
+        e.preventDefault();
+
+        var $link = $(e.currentTarget);
+
+        $.ajax({method:"POST", url: $link.attr('href')}).done(function(data){
+          $('.js-like-article-count').html(data.output);
+        });
+    });
+});

d1ca293ee0762568f722866c3d8375750babd04c
Stephane <apsarawebdev@protonmail.com>
2021-02-13 08:48:05 +0700

    lesson crud

---
+++ src/Form/LessonType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lesson;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LessonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('video')
+            ->add('content')
+            ->add('order_id')
+            ->add('title')
+            ->add('completed')
+            ->add('Cours')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Lesson::class,
+        ]);
+    }
+}

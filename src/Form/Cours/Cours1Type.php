e7315fcf963eda5c49998b00cbd2d7242ad7582f
Stephane <apsarawebdev@protonmail.com>
2021-02-14 11:36:47 +0700

    validation image

--- src/Form/Cours/Cours1Type.php
+++ src/Form/Cours/Cours1Type.php
@@ -6,6 +6,8 @@ use App\Entity\Cours;
 use Symfony\Component\Form\AbstractType;
 // use App\Form\Cours\CoursCardsImageFormType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\NotNull;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -15,14 +17,30 @@ class Cours1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $cours = $options['data'] ?? null;
+        $isEdit = $cours && $cours->getId(); 
+
         $builder
             ->add('titre', TextType::class)
             ->add('level', TextType::class)
             ->add('duration', TextType::class)
             ->add('description', TextareaType::class)
+        ;
+        $imageConstraints = [
+            new Image([
+                'maxSize' => '5M',
+            ])
+        ];
+        if (!$isEdit || !$cours->getMainImage()) {
+            $imageConstraints[] = new NotNull([
+                'message' => 'Veuillez charger une image svp',
+            ]);
+        }
+        $builder
             ->add('mainImage', FileType::class, [
                 'mapped' => false,
-                'required' => false // provisoire 
+                'required' => false,
+                'constraints' => $imageConstraints
             ])
         ;
     }

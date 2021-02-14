2305552a43ca7b45d886bf166ae74ed4aa115592
Stephane <apsarawebdev@protonmail.com>
2021-02-14 10:15:01 +0700

    anchor from lesson

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -1,3 +1,21 @@
+// add params 
+$('#cours-parent').on('click', function() {
+    sessionStorage.setItem('parent', true);
+});
+// scroll to lesson when come back to cours_edit page from lesson_edit
+$(document).ready(function() {
+    var data = sessionStorage.getItem('parent');
+    if (data !== null) {
+        // scroll to lessons
+        let scrollTop = $('#table-test').offset().top;
+        window.scrollTo(0, scrollTop - 100);
+        // Supprimer des données de sessionStorage
+        sessionStorage.removeItem('parent');
+        // Supprimer toutes les données de sessionStorage
+        sessionStorage.clear();
+        return false;
+    }
+});
 // hide flash message after 5s 
 $(function() {
     let message = $('.alert-success');
@@ -8,19 +26,23 @@ $(function() {
     }
 });
 
-// order ********
+// order 
 $(function() {
     $(document).on( "click", ".btn-position", function(e) {
         e.preventDefault();
         let id = '#form-' + $(this).attr('data-url');
         let link = $(id).attr('action');
+        // submit form 
         $.ajax({url: link, 
             method:"POST",
             url: link,
-            data: $(id).serialize(), // our data object
+            data: $(id).serialize(), 
+        // load list 
         }).success(function(data) {
             $("#table-test").html();
             $("#table-test").load("/lesson/"+data[2]+"/load");
         });
     });
 });
+
+
